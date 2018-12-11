c9f2fc2b9ec885b9a990cffb8cae822a15ec994c
Peter-G-Stone <peter.stone.online@gmail.com>
2018-12-11 14:55:16 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  #this makes the homepage open the posts/index route, but without changing the homepage url
   root 'posts#index'
 end
